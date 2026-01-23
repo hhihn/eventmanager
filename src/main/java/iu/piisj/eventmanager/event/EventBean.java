24c0b3c23de902304d2151f8af2826eac96186ab
hhihn <heinkehihn@gmail.com>
2026-01-23 13:54:15 +0100

    implement event saving

--- src/main/java/iu/piisj/eventmanager/event/EventBean.java
+++ src/main/java/iu/piisj/eventmanager/event/EventBean.java
@@ -1,41 +1,54 @@
 package iu.piisj.eventmanager.event;
-
-import iu.piisj.eventmanager.dto.Event;
-import jakarta.annotation.PostConstruct;
+import iu.piisj.eventmanager.dto.EventDTO;
+import iu.piisj.eventmanager.repository.EventRepository;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.application.FacesMessage;
 import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
-
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 @Named
 @ViewScoped
 public class EventBean implements Serializable {
 
-    private List<Event> events;
-    private Event newEvent;
+    @Inject
+    private EventRepository eventRepository;
+    private EventDTO newEvent = new EventDTO();
 
-    @PostConstruct
-    public void init() {
-
-        newEvent = new Event("", "", "", "Geplant");
-    }
-
-    public List<Event> getEvents() {
-        return events;
-    }
-
-    public Event getNewEvent() {
-        return newEvent;
+    public List<Event> getAllEvents() {
+        return eventRepository.findAll();
     }
 
     public List<String> getAvailableStatuses() {
         return List.of("Geplant", "Offen", "Abgeschlossen");
     }
 
-    public void addEvent() {
-        events.add(newEvent);
-        newEvent = new Event("", "", "", "Geplant");
+    public void saveEvent() {
+        Event eventEntity = mapToEntity(newEvent);
+        eventRepository.save(eventEntity);
+        FacesMessage message = new FacesMessage(
+                FacesMessage.SEVERITY_INFO,
+                "Erfolg! Die Veranstaltung wurde erfolgreich angelegt.",
+                eventEntity.toString()
+        );
+
+        FacesContext.getCurrentInstance().addMessage(null, message);
+        // Formular zurücksetzen
+        newEvent = new EventDTO();
+    }
+
+    private Event mapToEntity(EventDTO dto) {
+        return new Event(
+                dto.getName(),
+                dto.getLocation(),
+                dto.getDate(),
+                dto.getState()
+        );
+    }
+
+    public EventDTO getNewEvent() {
+        return newEvent;
     }
 }
