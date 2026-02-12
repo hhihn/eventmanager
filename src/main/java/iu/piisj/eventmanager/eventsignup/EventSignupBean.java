04f2e18e48a576762b44f49bf723712d68480fa6
hhihn <heinkehihn@gmail.com>
2026-02-12 16:36:40 +0100

    implemented signupbean functions

--- src/main/java/iu/piisj/eventmanager/eventsignup/EventSignupBean.java
+++ src/main/java/iu/piisj/eventmanager/eventsignup/EventSignupBean.java
@@ -1,11 +1,13 @@
 package iu.piisj.eventmanager.eventsignup;
 
+import iu.piisj.eventmanager.event.Event;
 import iu.piisj.eventmanager.repository.EventSignupRepository;
 import iu.piisj.eventmanager.service.EventSignupService;
 import iu.piisj.eventmanager.usermanagement.User;
 import iu.piisj.eventmanager.event.EventDetailBean;
 import iu.piisj.eventmanager.usermanagement.UserLoginBean;
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
@@ -68,4 +70,85 @@ public class EventSignupBean  implements Serializable {
         }
     }
 
+    public String signupUserForEvent(){
+        User currentUser = userLoginBean.getLoggedInUser();
+        if (currentUser == null){
+            facesContext.addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_WARN,
+                            "Bitte anmelden.", null));
+            return "redirect:login";
+        }
+
+        Event currentEvent = eventDetailBean.getSelectedEvent();
+        if (currentEvent == null) {
+            facesContext.addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_WARN,
+                            "Event nicht gefunden.", null));
+            return null;
+        }
+        // implementierung der geschäftslogik im service
+        // hier im bean ist nur die kommunikation implementiert
+        boolean success = eventSignupService.signupUserForEvent(
+                currentUser.getId(),
+                currentEvent.getId()
+        );
+
+        if (success){
+            facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
+                    "Sie haben sich erfolgreich für dieses Event registriert.", null));
+            isUserSignedUp = true;
+            loadSignups();
+            eventDetailBean.loadEvent();
+        } else {
+            facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Registrierung fehlgeschlagen.", null));
+        }
+        return null;
+    }
+
+    public void cancelSignup(){
+        User currentUser = userLoginBean.getLoggedInUser();
+        if (currentUser == null){
+            return;
+        }
+
+        Event currentEvent = eventDetailBean.getSelectedEvent();
+        if (currentEvent == null) {
+            facesContext.addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Event nicht gefunden", null));
+            return;
+        }
+
+        boolean success = eventSignupService.cancelSignup(
+                currentUser.getId(),
+                currentEvent.getId()
+        );
+
+        if (success) {
+            facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
+                    "Erfolgreich vom Event abgemeldet", null));
+            isUserSignedUp = false;
+            loadSignups();
+            eventDetailBean.loadEvent();
+        } else {
+            facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
+                    "Abmelden war nicht erfolgreich.", null));
+        }
+    }
+
+    public boolean isUserSignedUp() {
+        return isUserSignedUp;
+    }
+
+    public void setUserSignedUp(boolean userSignedUp) {
+        isUserSignedUp = userSignedUp;
+    }
+
+    public List<EventSignup> getEventSignups() {
+        return eventSignups;
+    }
+
+    public void setEventSignups(List<EventSignup> eventSignups) {
+        this.eventSignups = eventSignups;
+    }
 }
